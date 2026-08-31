028e9236995cb1e9de2047ef31e515122acf5d30
alexpmtk-afk <alexpmtk@gmail.com>
2026-08-31 13:18:12 +0300

    BIRZHA: grant secret revision deployment permission

--- terraform/birzha_test/runtime_deployer_iam.tf
+++ terraform/birzha_test/runtime_deployer_iam.tf
@@ -4,3 +4,10 @@ resource "yandex_iam_service_account_iam_member" "terraform_runtime_user" {
   member             = "serviceAccount:${var.terraform_deployer_service_account_id}"
   sleep_after        = 10
 }
+
+resource "yandex_resourcemanager_folder_iam_member" "terraform_secret_revision_editor" {
+  folder_id   = yandex_resourcemanager_folder.birzha_test.id
+  role        = "functions.editor"
+  member      = "serviceAccount:${var.terraform_deployer_service_account_id}"
+  sleep_after = 30
+}
